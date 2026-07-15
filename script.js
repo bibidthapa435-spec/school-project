661c2360f064ad37a84b1147956bc310ed0516c6
Bishuthapa <our-email@example.com>
2026-07-15 13:30:29 +0545

    update the file

--- script.js
+++ script.js
@@ -543,37 +543,3 @@ window.addEventListener("scroll", function () {
 /* login portal */
 
 
-
-document.addEventListener("DOMContentLoaded", function () {
-
-    const slides = document.querySelectorAll(".slide");
-
-    let current = 0;
-
-    function changeSlide() {
-        slides[current].classList.remove("active");
-
-        current = (current + 1) % slides.length;
-
-        slides[current].classList.add("active");
-    }
-
-    setInterval(changeSlide, 10000);
-
-});
-window.addEventListener("scroll", function () {
-
-    const navbar = document.getElementById("navbar-container");
-
-    if (window.scrollY > 200) {
-        navbar.classList.add("sticky");
-    } else {
-        navbar.classList.remove("sticky");
-    }
-
-});
-
-
-
-/* login portal */
-
