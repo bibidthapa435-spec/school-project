30a47e530d20b6fbaf864ca577e5d31340c453c2
bibidthapa435-spec <bibidthapa435@gmail.com>
2026-08-22 22:44:36 +0545

    change

--- school_app/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class SchoolAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'school_app'
-    verbose_name = 'School Management'
--- school_app/context_processors.py
+++
@@ -1,8 +0,0 @@
-from .models import SiteSettings
-
-
-def site_settings(request):
-    settings = SiteSettings.objects.first()
-    if settings is None:
-        settings = SiteSettings.objects.create()
-    return {'site_settings': settings}
